7a9dfcc08255abf526da9520ac54d800f50d9a52
nluskarev <nluskarev@nluskarev-ub22-24.22>
2024-04-04 11:37:18 +0300

    Добавлена форма для заполнения людей

--- src/components/PersList.vue
+++ src/components/PersList.vue
@@ -1,14 +1,29 @@
 
 
 <script setup>
-import FoodList from  'src/components/FoodList.vue'
+
+import FoodList from './FoodList.vue'
+
 
 
 </script>
 
 <template>
     <div>
+        
+    <title>Vue.js Input Output</title>
+   
+
+
+    <div id="app">
+        <input type="text" v-model="name" @input="updateList">
+        <ul>
+            <li v-for="(item, index) in names" :key="index">{{ item }}</li>
+        </ul>
+    </div>
 <ul>Persons</ul>
 <FoodList/>
+
     </div>
+
 </template>
