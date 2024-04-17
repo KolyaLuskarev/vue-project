0959b683a8f7b150cf7a9f65f6a58cedb867bd23
KolyaLuskarev <kolalus2000@gmail.com>
2024-04-17 14:06:31 +0300

    Работа со стилями

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,34 @@
+ <script setup>
+const props = defineProps({
+
+    imgUrl: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+ <template>
+    <div class="root">
+        <div :style="`background-image: url( ${imgUrl} )`" class="img"></div>
+        <div class="main"></div>
+    </div>
+ </template>
+
+ <style lang="sass" scoped>
+
+@import '../assets/styles/main'
+
+.root
+    displey: flex
+    min-height: 100vh
+    background-color: $background
+.img
+    width: 50px
+    background-position: 50px 50px
+    background-repeat: no-repeat
+    background-size: cover
+.main
+    width: 50px
+
+</style>
