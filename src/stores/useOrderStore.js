0d82650d7340bbc29909367de076cf811a058d94
KolyaLuskarev <kolalus2000@gmail.com>
2024-05-04 22:30:59 +0300

    Устранена проблема с маршрутизацией

--- src/stores/useOrderStore.js
+++ src/stores/useOrderStore.js
@@ -4,19 +4,22 @@ import { defineStore } from 'pinia'
 
 export const useOrderStore = defineStore('foodList', {
   state: () => ({
-    /** @type {{ foodName: string,  foodPrise: number, foodID: number }[]} */
+    /** @type {{ foodName: string,  foodPrise: number,  custNames: [], orderID: number}[]} */
     foodList: [],
     orderID: 0,
-    foodID: 0,
-    /** @type {{ name: string , orderID: number }[]} */
-    names: [],
+   
+
   }),
   actions: {
-    addFood(foodName, foodPrise, newNames) {
-      this.foodList.push({ foodName, foodPrise,  foodID: this.foodID++ })
-      this.names.concat({newNames}) 
-      this.names.push({  orderID: this.orderID++ })
-      
+    addFood(foodName, foodPrise, custNames) {
+      this.foodList.push(
+        { foodName,
+          foodPrise,
+          custNames,
+          orderID: this.orderID++,
+          payerName: '',
+        })
+     
     },
   },
 })
